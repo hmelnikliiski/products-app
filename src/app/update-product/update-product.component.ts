4414240251260eaefa23f94a6b4c022a8d73d7d2
Hristo Melnikliyski <hmelnikliiski@gmail.com>
2018-06-12 16:49:41 +0300

    Editing product

---
+++ src/app/update-product/update-product.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+import { ProductsService } from '../shared/products.service';
+
+@Component({
+  selector: 'app-update-product',
+  templateUrl: './update-product.component.html',
+  styleUrls: ['./update-product.component.css']
+})
+export class UpdateProductComponent {
+
+  @Input() uname: string;
+  @Input() uprice: number;
+
+  constructor(private _productsService: ProductsService) {
+  }
+
+  updateProduct(name: string, price: number) {
+    this._productsService.updateProduct(name, price);
+  }
+}
